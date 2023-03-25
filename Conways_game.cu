3dcf92e749a3a5f2c6f909c07668023d23d9693e
wyh <1123858599@qq.com>
2023-03-25 12:31:47 +0800

    优化了变量命名，增加了少量注释

--- Conways_game.cu
+++ Conways_game.cu
@@ -11,12 +11,12 @@
 #define HEIGHT 900
 // 颜色色域256
 #define COLOR_RANGE 256
-#define HZ 75
+#define HZ 144
 #define USE_GPU 1
 
 // 细胞状态，0 表示死亡，1 表示存活
 int h_cellls[2][HEIGHT][WIDTH] = { 0 };
-int now = 0;
+int now = 0; //当前帧对应的数组id，上一帧对应的id为pre = now ^ 1;
 int* d_cells[2];
 
 
@@ -34,7 +34,7 @@ void init_cells() {
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
     // 蓝色
-    // glColor3f(1.0 / COLOR_RANGE, 149.0 / COLOR_RANGE, 249.0 / COLOR_RANGE);
+    //glColor3f(1.0 / COLOR_RANGE, 149.0 / COLOR_RANGE, 249.0 / COLOR_RANGE);
     // 绿色
     glColor3f(0.0 / COLOR_RANGE, 197.0 / COLOR_RANGE, 50.0 / COLOR_RANGE);
     glPointSize(1.0);
@@ -65,7 +65,7 @@ void update_cells() {
                 }
             }
         }
-    }
+    } 
     // 根据细胞状态和周围存活细胞数量更新细胞状态
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
@@ -74,25 +74,25 @@ void update_cells() {
     }
 }
 
-__global__ void update(int* d_old, int* d_new, int M, int N) {
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ void update(int* d_old, int* d_new, int width, int height) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (i >= N || j >= M) return;
+    if (y >= height || x >= width) return;
 
     int count = 0;
-    for (int p = i - 1; p <= i + 1; p++) {
-        for (int q = j - 1; q <= j + 1; q++) {
-            if (p >= 0 && p < N && q >= 0 && q < M && !(p == i && q == j)) {
-                count += d_old[p * M + q];
+    for (int p = y - 1; p <= y + 1; p++) {
+        for (int q = x - 1; q <= x + 1; q++) {
+            if (p >= 0 && p < height && q >= 0 && q < width && !(p == y && q == x)) {
+                count += d_old[p * width + q];
             }
         }
     }
     if (3<= count && count<=7) {
-        d_new[i * M + j] = 1;
+        d_new[y * width + x] = 1;
     }
     else {
-        d_new[i * M + j] = 0;
+        d_new[y * width + x] = 0;
     }
 }
 dim3 dimBlock(32, 32);
@@ -100,10 +100,10 @@ dim3 dimGrid((WIDTH + dimBlock.x - 1) / dimBlock.x, (HEIGHT + dimBlock.y - 1) / 
 
 // 计时器函数，每隔一定时间更新一次细胞状态，设置为144hz
 void timer(int value) {
-    now ^= 1;
+    now ^= 1; //now在0 1 之间不断转换
     int pre = now ^ 1;
     if (USE_GPU) {
-        update <<<dimGrid, dimBlock >>> (d_cells[pre], d_cells[now], WIDTH, HEIGHT);
+        update <<< dimGrid, dimBlock >>> (d_cells[pre], d_cells[now], WIDTH, HEIGHT);
         cudaMemcpy(h_cellls[now], d_cells[now], WIDTH * HEIGHT * sizeof(int), cudaMemcpyDeviceToHost);
     }
     else {
